8a7f6d6fc2c6b193c28c1a239919f22652e5a317
Sk. Razibul Islam <120094888+iamrazib@users.noreply.github.com>
2023-12-27 22:30:38 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Accordion from "./components/Accordion"
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="text-center text-3xl bg-red-200">Project 1</h1>
+      <Accordion />
+    </>
+  )
+}
+
+export default App
